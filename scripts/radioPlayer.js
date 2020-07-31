f9e98706b6acdb0249058177ea213fbf2f174ad5
Petr Markov <channelpitnik@gmail.com>
2020-07-31 23:02:54 +0500

    add radio volume change

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -6,11 +6,16 @@ export const radioPlayerInit = () =>{
     const radioNavigation = document.querySelector('.radio-navigation');
     const radioHeaderBig = document.querySelector('.radio-header__big');
     const radioItem = document.querySelectorAll('.radio-item');
-    
+    const radioVolume = document.querySelector('.radio-volum')
+	const radioIconValueOff = document.querySelector('.radio-icon-value-Off')
+	const playerBtn = document.querySelectorAll('.player-btn')
     const audio = new Audio();
-    audio.type = 'audio/aac';
 
+    audio.type = 'audio/aac';
     radioStop.disabled = true;
+    radioVolume.value = 50;
+
+    let radioVolumeValue;
     
     const changeIconPlay = () => {
         if (audio.paused) {
@@ -28,6 +33,11 @@ export const radioPlayerInit = () =>{
         radioItem.forEach(item => item.classList.remove('select'));
         element.classList.add('select');
     };
+
+    const changeRadioVolume = value => {
+		audio.volume = value / 100;
+		radioVolum.value = value;
+	};
 
     radioNavigation.addEventListener('change', event => {
         const target = event.target;
@@ -51,4 +61,34 @@ export const radioPlayerInit = () =>{
         (audio.paused) ? audio.play() :  audio.pause();
         changeIconPlay();
     });
+
+    radioVolume.addEventListener('input', () => {
+		audio.volume = radioVolume.value / 100
+    });
+    
+    radioIconValueOff.addEventListener('click', () => {
+		if (audio.volume != 0 ){
+			radioVolumeValue = radioVolume.value;
+			changeRadioVolume(0);
+			radioIconValueOff.classList.add('fa-volume-off');
+			radioIconValueOff.classList.remove('fa-volume-down');
+
+		} else {
+
+			changeRadioVolume(radioVolumeValue);
+			radioIconValueOff.classList.remove('fa-volume-off');
+			radioIconValueOff.classList.add('fa-volume-down');
+		}
+    });
+    
+    playerBtn[0].addEventListener('click', () =>{
+		audio.pause();
+		changeIconPlay();
+	});
+
+	playerBtn[1].addEventListener('click', () =>{
+		audio.pause();
+		changeIconPlay();
+	});
+    
 };
